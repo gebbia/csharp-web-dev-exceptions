f328a1f453f74dd07bea6f31c481909679d3e3de
Gebbia <gebbianatasha@gmail.com>
2022-10-04 03:13:29 -0500

    Exercises Completed

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,54 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+       
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0.0)
+            {
+                throw new ArgumentOutOfRangeException("y", "0 should not be entered for the total number of possible points.");
+            }
+            else
+            {
+                return x / y;
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("fileName", "Student did not properly submit a file!");
+            }
+            else
+            {
+                if (fileName.Substring(fileName.Length - 3, 3) == ".cs")
+                { 
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
         }
-        */
+       
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            double num1 = 9.9;
+            double num2 = 0.0;
+
+            try
+            {
+                Divide(num1, num2);
+            }
+            catch (ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
@@ -30,7 +63,18 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            foreach (KeyValuePair<string, string> student in students)
+            {
+                try
+                {
+                    Console.WriteLine(student.Key);
+                    CheckFileExtension(student.Value);
+                }
+                catch (ArgumentNullException n)
+                {
+                    Console.WriteLine(n.Message);
+                }
+            }
         }
     }
 }
